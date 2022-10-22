8cf08044c48a65112def59a97a0cdd20a755b8cb
DeN2828 <den-tu@ua.fm>
2022-10-22 16:18:56 +0300

    Version 1.0.1

--- calculate braking distance/calculate braking distance/Calculate.cs
+++ calculate braking distance/calculate braking distance/Calculate.cs
@@ -8,5 +8,23 @@ namespace calculate_braking_distance
 {
     internal class Calculate
     {
+        float k { get; set; }
+        float f { get; set; }
+        float v { get; set; }
+        float s { get; set; }
+
+        public Calculate(float k, float f, float v)
+        {
+            this.k = k;
+            this.f = f;
+            this.v = v;
+            s = 0F;
+        }
+
+        public string Calculates()
+        {
+            s = (k / (254 * f)) * v * v;
+            return $"Тормозной путь = {s} meters";
+        }
     }
 }
--- calculate braking distance/calculate braking distance/Program.cs
+++ calculate braking distance/calculate braking distance/Program.cs
@@ -1,4 +1,13 @@
 ﻿using calculate_braking_distance;
 
-Calculate calculate = new Calculate(1F, 0.2F, 100);
-Console.WriteLine(calculate.Calculates());
+Console.WriteLine("Enter coating condition: (0,1 - icy; 0,2 - snowy; 0,4 - wet; 0,7 - dry)");
+float input = float.Parse(Console.ReadLine());
+if (input == 0.1F || input == 0.2F || input == 0.4F || input == 0.7F)
+{
+    Calculate calculate = new Calculate(1F, input, 100);
+    Console.WriteLine(calculate.Calculates());
+}
+else
+{
+    Console.WriteLine("Error");
+}
