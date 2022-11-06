b913979a8832431d189d1fa5824b65ea035ff963
DeN2828 <den-tu@ua.fm>
2022-11-06 18:56:07 +0200

    Version 1.0.0.4

--- calculate braking distance/calculate braking distance/Calculate.cs
+++ calculate braking distance/calculate braking distance/Calculate.cs
@@ -11,7 +11,10 @@ namespace calculate_braking_distance
         float k { get; set; }
         float f { get; set; }
         float v { get; set; }
-        float s { get; set; }
+        public float s { get; set; }
+        public float number;
+        public float Speed;
+
 
         public Calculate(float k, float f, float v)
         {
@@ -20,7 +23,47 @@ namespace calculate_braking_distance
             this.v = v;
             s = 0F;
         }
+        public Calculate()
+        {
+        }
 
+        
+
+        public void GetResult()
+        {
+                Console.WriteLine("Enter num (1 - icy; \n2 - snowy; \n3 - wet; \n4 - dry \n0 - Exit): ");
+                int num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        number = 0.1F;
+                        break;
+                    case 2:
+                        number = 0.2F;
+                        break;
+                    case 3:
+                        number = 0.4F;
+                        break;
+                    case 4:
+                        number = 0.7F;
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+
+                Console.WriteLine("Enter speed (1 - 500): ");
+                float.TryParse(Console.ReadLine(), out var speed);
+                if (speed < 1 && speed > 500)
+                {
+                    Console.WriteLine("Error enter speed");
+                    Environment.Exit(0);
+                }
+                Speed = speed;
+        }
         public string Calculates()
         {
             s = (k / (254 * f)) * v * v;
